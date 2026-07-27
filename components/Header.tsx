dc32ec3f74d004a7dd177e59c5ee7e3cfcec9b31
Solar138 <46548002+solar138@users.noreply.github.com>
2026-07-27 12:06:06 -0700

    add navigation links to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,19 @@
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Header({ children } : { children : React.ReactNode }) {
-    return <header className="flex-none h-14 border-b px-4 flex items-center bg-white z-10 border-gray-200 text-2xl font-bold">{children}</header>
-}
+    const pathname = usePathname();
+    const parts = pathname.split("/");
+
+    return <header className="h-14 border-b px-4 flex items-center bg-white z-10 border-gray-200 text-2xl font-bold w-full">
+        {children}
+        <div className="m-auto"></div>
+        <div className="float-right flex gap-10 font-normal header-nav">
+            <Link href="scheduler" className={parts[2] == "scheduler" ? "active" : ""}>Scheduler</Link>
+            <Link href="courses" className={parts[2] == "courses" ? "active" : ""}>Explorer</Link>
+            <Link href="planner" className={parts[2] == "planner" ? "active" : ""}>Planner</Link>
+        </div>
+    </header>
+} 
