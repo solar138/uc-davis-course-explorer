83dfe690d3e92c85e64a69a1411c7f7feb6c4172
Solar138 <46548002+solar138@users.noreply.github.com>
2026-08-03 01:04:01 -0700

    move changelog github link to the top

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -5,29 +5,29 @@ export default async function CourseExplorer() {
 
   return (
     <div className="flex flex-col w-full h-screen bg-white">
-      <Header> Changelog</Header> 
+      <Header> Changelog</Header>
       <main className="flex-1 flex min-h-0 overflow-y-scroll">
         <article className="p-4 text-justify w-full max-w-2xl h-fit mx-auto gap-4 flex flex-col">
-          <ChangelogEntry date="Aug 2, 2026" changes={["Fix course section display issues", "Fixed some typos in the changelog"]}/>
-          <ChangelogEntry date="July 28, 2026" changes={["Add transfer credit tab to degree planner"]}/>
-          <ChangelogEntry date="July 27, 2026" changes={["Add navigation tabs to header"]}/>
-          <ChangelogEntry date="July 25, 2026" changes={["Reorganized URLs to [school]/[app]. Old URLs will redirect for now.", "Renamed some things", "Should use less bandwidth now"]}/>
-          <ChangelogEntry date="July 24, 2026" changes={["NEW: Degree planner, currently supports degree program selection and entering exam credits", "Course requirements planning coming soon"]}/>
-          <ChangelogEntry date="July 23, 2026" changes={["Clicking on a scheduler block now shows it's sections", "Section list for a scheduled course highlights the current section", "Course name now shown on top of section list"]}/>
-          <ChangelogEntry date="July 20, 2026" changes={["User data now persists locally. Logins coming soon for storing your data on the cloud", "Reorganized header"]}/>
-          <ChangelogEntry date="July 16, 2026" changes={["Sections now preview meetings when hovering over their list entries", "Some potential bandwidth and performance improvements", "Fixed: Duplicate scheduler error messages"]}/>
-          <ChangelogEntry date="July 14, 2026" changes={["Added error messages when scheduling instead of silently failing", "Changed wording on course inspector", "Added unschedule button in section list", "Improved scheduler performance"]}/>
-          <ChangelogEntry date="July 13, 2026" changes={["Added changelog", "Reduced bandwidth usage by not sending the entire course library", "Course unlocks temporarily hidden", "Fixed: SVG icons not loading in inspector", "Fixed: Courses showing internal IDs instead of the course code."]}/>
-          <ChangelogEntry date="July 10, 2026" changes={["Initial release"]}/>
-        <p>For more detailed changes, feel free to visit the <a href="https://www.github.com/4thharbinger/uc-davis-course-explorer/commits">GitHub commits</a></p>
+          <p>For more detailed changes, feel free to visit the <a href="https://www.github.com/4thharbinger/uc-davis-course-explorer/commits">GitHub commits</a></p>
+          <ChangelogEntry date="Aug 2, 2026" changes={["Fix course section display issues", "Fixed some typos in the changelog"]} />
+          <ChangelogEntry date="July 28, 2026" changes={["Add transfer credit tab to degree planner"]} />
+          <ChangelogEntry date="July 27, 2026" changes={["Add navigation tabs to header"]} />
+          <ChangelogEntry date="July 25, 2026" changes={["Reorganized URLs to [school]/[app]. Old URLs will redirect for now.", "Renamed some things", "Should use less bandwidth now"]} />
+          <ChangelogEntry date="July 24, 2026" changes={["NEW: Degree planner, currently supports degree program selection and entering exam credits", "Course requirements planning coming soon"]} />
+          <ChangelogEntry date="July 23, 2026" changes={["Clicking on a scheduler block now shows it's sections", "Section list for a scheduled course highlights the current section", "Course name now shown on top of section list"]} />
+          <ChangelogEntry date="July 20, 2026" changes={["User data now persists locally. Logins coming soon for storing your data on the cloud", "Reorganized header"]} />
+          <ChangelogEntry date="July 16, 2026" changes={["Sections now preview meetings when hovering over their list entries", "Some potential bandwidth and performance improvements", "Fixed: Duplicate scheduler error messages"]} />
+          <ChangelogEntry date="July 14, 2026" changes={["Added error messages when scheduling instead of silently failing", "Changed wording on course inspector", "Added unschedule button in section list", "Improved scheduler performance"]} />
+          <ChangelogEntry date="July 13, 2026" changes={["Added changelog", "Reduced bandwidth usage by not sending the entire course library", "Course unlocks temporarily hidden", "Fixed: SVG icons not loading in inspector", "Fixed: Courses showing internal IDs instead of the course code."]} />
+          <ChangelogEntry date="July 10, 2026" changes={["Initial release"]} />
         </article>
       </main>
-      <Footer/>
-    </div>  
+      <Footer />
+    </div>
   );
 }
 
-function ChangelogEntry({changes, date, title} : {changes : string[], date : string, title? : string}) {
+function ChangelogEntry({ changes, date, title }: { changes: string[], date: string, title?: string }) {
   return <div>
     <h1 className="text-l font-bold">{date}</h1>
     <ul className="ml-4">
